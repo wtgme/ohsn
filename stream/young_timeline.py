ef92c88ee59bceb087cef1c5c106eebb38e1e78a
wtgmme <wtgmme@gmail.com>
2016-02-16 10:21:05 +0000

    add level control, compare the poi first.

--- stream/young_timeline.py
+++ stream/young_timeline.py
@@ -18,7 +18,8 @@ db = dbt.db_connect_no_auth('young')
 sample_user = db['com']
 sample_time = db['timeline']
 print datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")  + "\t" + 'Connecting db well'
-sample_user.create_index([('timeline_scraped_times', pymongo.ASCENDING)])
+sample_user.create_index([('timeline_scraped_times', pymongo.ASCENDING),
+                          ('level', pymongo.ASCENDING)])
 sample_time.create_index([('user.id', pymongo.ASCENDING),
                           ('id', pymongo.DESCENDING)])
 sample_time.create_index([('id', pymongo.ASCENDING)], unique=True)
