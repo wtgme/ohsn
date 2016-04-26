415bf205103b4f368720a05c35eb31612e8edbc2
wtgme <wtgmme@gmail.com>
2016-04-26 09:52:20 +0100

    add attribute to network

--- ohsn/util/graph_util.py
+++ ohsn/util/graph_util.py
@@ -7,8 +7,8 @@ Created on 20:05, 07/04/16
 
 from igraph import *
 import db_util as dbt
-import net_util as nt
-from time import time
+import plot_util as plot
+import pickle
 
 
 def load_network(db_name, collection='None'):
@@ -79,6 +79,14 @@ def load_beh_network(db_name, collection='None', target_set=None):
     return g
 
 
+def add_attribute(g, dbname, colname, name, field):
+    db = dbt.db_connect_no_auth(dbname)
+    com = db[colname]
+    for v in g.vs:
+        v[name] = com.find_one({'id': int(v['name'])}, [field])[field]
+         
+
+
 def giant_component(g, mode):
     com = g.clusters(mode=mode)
     print 'The processed network has components:', len(com)
@@ -99,64 +107,83 @@ def community(g, weighted=False):
 
 
 if __name__ == '__main__':
-
-    g = load_network('fed', 'snet')
-    gc = giant_component(g, 'WEAK')
-    summary(gc)
-    coms = community(gc)
-    clus = coms.as_clustering()
-    summary(clus)
-    for clu in clus:
-        summary(clu)
-
-    DG = nt.load_network('fed', 'snet')
-    gc2 = nt.get_gaint_comp(DG)
-    print gc2.number_of_nodes(), gc2.number_of_edges()
-
-    t0 = time()
-    g = load_network('fed', 'snet')
-    indegree1 = g.indegree()
-    outdegree1 = g.outdegree()
-    # plot.pdf_plot_one_data(indegree, 'indegree', min(indegree), max(indegree))
-    t1 = time()
-    DG = nt.load_network('fed', 'snet')
-    indegree2 = DG.in_degree().values()
-    outdegree2 = DG.out_degree().values()
-    # plot.pdf_plot_one_data(indegree, 'indegree', min(indegree), max(indegree))
-    t2 = time()
-    print list(set(indegree1) - set(indegree2))
-    print list(set(outdegree1) - set(outdegree2))
-    print 'function vers1 takes %f' %(t1-t0)
-    print 'function vers2 takes %f' %(t2-t1)
-
-    for v in DG.nodes():
-        ist1 = DG.in_degree(v)
-        ist2 = g.indegree(str(v))
-        if ist1 != ist2:
-            print 'Fa'
-    print 'finished'
+    # g = load_beh_network('fed', 'sbnet')
+    # pickle.dump(g, open('data/bg.pick', 'w'))
+    g = pickle.load(open('data/bg.pick', 'r'))
+    for v in g.vs:
+        print v['name']
+    # print g.es[g.get_eid('480706562', '386203927')]
+    # print g.es[g.get_eid('1092937046', '777200318')]
+    # print g.es[g.get_eid('1129480146', '919267472')]
+    # print g.es[g.get_eid('431013874', '518335800')]
+    # print g.es[g.get_eid('249683957', '548884928')]
+    # indegree = g.indegree()
+    # outdegree = g.outdegree()
+    # instrength = g.strength(mode='IN', weights='weight')
+    # outstrenght = g.strength(mode='OUT', weights='weight')
+    # print min(indegree), max(indegree)
+    # plot.pdf_plot_one_data(indegree, 'Indegree')
+    # plot.pdf_plot_one_data(outdegree, 'Outdegree')
 
 
-    g = load_beh_network('fed', 'sbnet')
-    indegree1 = g.indegree()
-    outdegree1 = g.outdegree()
-    inst1 = g.strength(mode='IN')
-    outst1 = g.strength(mode='OUT')
 
-    DG = nt.load_behavior_network('fed', 'sbnet')
-    indegree2 = DG.in_degree().values()
-    outdegree2 = DG.out_degree().values()
-    inst2 = DG.in_degree(weight='weight').values()
-    outst2 = DG.out_degree(weight='weight').values()
-
-    print list(set(indegree1) - set(indegree2))
-    print list(set(outdegree1) - set(outdegree2))
-    print list(set(inst1) - set(inst2))
-    print list(set(outst1) - set(outst2))
-
-    for v in DG.nodes():
-        ist1 = DG.in_degree(v, weight='weight')
-        ist2 = g.strength(str(v), mode='IN', weights='weight')
-        if ist1 != ist2:
-            print ist1, ist2
-            # print 'Fa'
+    # g = load_network('fed', 'snet')
+    # gc = giant_component(g, 'WEAK')
+    # summary(gc)
+    # coms = community(gc)
+    # clus = coms.as_clustering()
+    # summary(clus)
+    # for clu in clus:
+    #     summary(clu)
+    #
+    # DG = nt.load_network('fed', 'snet')
+    # gc2 = nt.get_gaint_comp(DG)
+    # print gc2.number_of_nodes(), gc2.number_of_edges()
+    #
+    # t0 = time()
+    # g = load_network('fed', 'snet')
+    # indegree1 = g.indegree()
+    # outdegree1 = g.outdegree()
+    # # plot.pdf_plot_one_data(indegree, 'indegree', min(indegree), max(indegree))
+    # t1 = time()
+    # DG = nt.load_network('fed', 'snet')
+    # indegree2 = DG.in_degree().values()
+    # outdegree2 = DG.out_degree().values()
+    # # plot.pdf_plot_one_data(indegree, 'indegree', min(indegree), max(indegree))
+    # t2 = time()
+    # print list(set(indegree1) - set(indegree2))
+    # print list(set(outdegree1) - set(outdegree2))
+    # print 'function vers1 takes %f' %(t1-t0)
+    # print 'function vers2 takes %f' %(t2-t1)
+    #
+    # for v in DG.nodes():
+    #     ist1 = DG.in_degree(v)
+    #     ist2 = g.indegree(str(v))
+    #     if ist1 != ist2:
+    #         print 'Fa'
+    # print 'finished'
+    #
+    #
+    # g = load_beh_network('fed', 'sbnet')
+    # indegree1 = g.indegree()
+    # outdegree1 = g.outdegree()
+    # inst1 = g.strength(mode='IN')
+    # outst1 = g.strength(mode='OUT')
+    #
+    # DG = nt.load_behavior_network('fed', 'sbnet')
+    # indegree2 = DG.in_degree().values()
+    # outdegree2 = DG.out_degree().values()
+    # inst2 = DG.in_degree(weight='weight').values()
+    # outst2 = DG.out_degree(weight='weight').values()
+    #
+    # print list(set(indegree1) - set(indegree2))
+    # print list(set(outdegree1) - set(outdegree2))
+    # print list(set(inst1) - set(inst2))
+    # print list(set(outst1) - set(outst2))
+    #
+    # for v in DG.nodes():
+    #     ist1 = DG.in_degree(v, weight='weight')
+    #     ist2 = g.strength(str(v), mode='IN', weights='weight')
+    #     if ist1 != ist2:
+    #         print ist1, ist2
+    #         # print 'Fa'
