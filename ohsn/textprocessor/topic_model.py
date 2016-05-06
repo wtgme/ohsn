58ea3d803e9a72a4eac4b8968f05fe3a2856eb92
wtgme <wtgmme@gmail.com>
2016-05-06 10:54:02 +0100

    topic model

--- ohsn/textprocessor/topic_model.py
+++ ohsn/textprocessor/topic_model.py
@@ -4,14 +4,17 @@ Created on 4:52 PM, 5/2/16
 
 @author: tw
 """
-
 from gensim import corpora, models
 from ohsn.util import db_util as dbt
+import pickle
 import re
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
+from nltk import pos_tag
 from nltk.stem.snowball import EnglishStemmer
+import logging
 
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 def read_document(dbname, colname, timecol, uset=None):
     db = dbt.db_connect_no_auth(dbname)
@@ -40,30 +43,41 @@ def read_document(dbname, colname, timecol, uset=None):
             if not(text.endswith('.') or text.endswith('?') or text.endswith('!')):
                 text += '.'
             textmass = textmass + " " + text.lower()
-        documents.append(textmass)
+        words = textmass.split()
+            # Any text with fewer than 50 words should be looked at with a certain degree of skepticism.
+        if len(words) > 50:
+            documents.append(textmass)
     return documents
 
 stopwds = stopwords.words('english')
 stemmer = EnglishStemmer()
+tokenizer = RegexpTokenizer(r'\w+')
 
 
 def pro_process_text(text):
     text = text.lower()
-    tokenizer = RegexpTokenizer(r'\w+')
     tokens = tokenizer.tokenize(text)
+    words = pos_tag(tokens)
     new_token = list()
-    for token in tokens:
-        if token not in stopwds:
-            try:
-                st = stemmer.stem(token)
-            except UnicodeDecodeError:
-                continue
-            new_token.append(st)
+    for word in words:
+        if word[1] is 'NN' or word[1] is 'JJ':
+            token = word[0]
+            if token not in stopwds:
+                try:
+                    st = stemmer.stem(token)
+                except UnicodeDecodeError:
+                    continue
+                new_token.append(st)
     return new_token
 
 
 def pre_process(documents):
-    texts = [[word for word in pro_process_text(document)] for document in documents]
+    texts = list()
+    for document in documents:
+        text = pro_process_text(document)
+        if len(text) > 0:
+            texts.append(text)
+    # texts = [[word for word in pro_process_text(document)] for document in documents]
     from collections import defaultdict
     frequency = defaultdict(int)
     for text in texts:
@@ -80,12 +94,14 @@ def pre_process(documents):
 def topic_model(dbname, colname, timecol, uset=None):
     documents = read_document(dbname, colname, timecol, uset)
     print len(documents)
+    pickle.dump(documents, open('data/document.pick', 'w'))
+    # documents = pickle.load(open('data/document.pick', 'r'))
     corpus, dictionary = pre_process(documents)
-    # from pprint import pprint
-    # print (dictionary.token2id)
-    # pprint(corpus)
-    lda = models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=20, update_every=1, chunksize=10000, passes=1)
-    lda.print_topics(num_topics=20, num_words=20)
+    pickle.dump((corpus, dictionary), open('data/corpus.pick', 'w'))
+    corpus, dictionary = pickle.load(open('data/corpus.pick', 'r'))
+    lda = models.ldamodel.LdaModel(corpus=corpus, num_topics=20, id2word=dictionary)
+    lda.print_topics(num_topics=20, num_words=100)
+
 
 if __name__ == '__main__':
     # print pro_process_text('A survey of user opinion of computer system response time')
