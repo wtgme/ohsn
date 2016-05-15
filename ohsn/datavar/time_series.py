89783346c0881d9d55f53bea8bcb3655a43ae1db
wtgmme <wtgmme@gmail.com>
2016-05-15 09:29:19 +0100

    t2

--- ohsn/datavar/time_series.py
+++ ohsn/datavar/time_series.py
@@ -124,10 +124,10 @@ if __name__ == '__main__':
     # textp.process_poi(dbname, tmpcom)
 
     '''Core ED'''
-    dbname, tmpcom, tmptimeline, tmpbnet = 'fed', 'com', 'timeline', 'fedbnet'
-    analysis(dbname, tmpbnet, tmpcom, 'fedsbnet', 'fedsnet', tmptimeline)
-    fridp.ed_pro(dbname, 'scom', tmpcom, 'net')
-    process('fed', 'timeline', [2009, 2010, 2011], 1, 'fed')
+    # dbname, tmpcom, tmptimeline, tmpbnet = 'fed', 'com', 'timeline', 'fedbnet'
+    # analysis(dbname, tmpbnet, tmpcom, 'fedsbnet', 'fedsnet', tmptimeline)
+    # fridp.ed_pro(dbname, 'scom', tmpcom, 'net')
+    # process('fed', 'timeline', [2009, 2010, 2011], 1, 'fed')
     process('fed', 'timeline', [2012], 2, 'fed')
     process('fed', 'timeline', [2013], 3, 'fed')
     process('fed', 'timeline', [2014], 4, 'fed')
