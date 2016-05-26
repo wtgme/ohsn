6debd47f05b6e323fdd497bd8d1f0e3c2f5dacb8
wtgme <wtgmme@gmail.com>
2016-05-26 13:07:01 +0100

    continue search tweets

--- ohsn/stream/search_tweets.py
+++ ohsn/stream/search_tweets.py
@@ -11,6 +11,7 @@ sys.path.append(path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
 import ohsn.util.db_util as dbt
 import ohsn.api.search as search_api
 import pymongo
+import time
 
 
 if __name__ == '__main__':
@@ -19,4 +20,12 @@ if __name__ == '__main__':
     search.create_index([('user.id', pymongo.ASCENDING),
                           ('id', pymongo.DESCENDING)])
     search.create_index([('id', pymongo.ASCENDING)], unique=True)
-    search_api.search_query('#MyDepressionLooksLike', search)
+    while True:
+        count1 = search.count()
+        search_api.search_query('#MyDepressionLooksLike', search)
+        count2 = search.count()
+        if count2 > count1:
+            time.sleep(60*60*12)
+            continue
+        else:
+            break
