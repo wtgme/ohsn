a59735271bb1d02247f659f336e3e0ff72b599a3
wtgmme <wtgmme@gmail.com>
2016-06-24 17:41:31 +0100

    modified:   ../networkminer/net_stat.py

--- ohsn/networkminer/net_stat.py
+++ ohsn/networkminer/net_stat.py
@@ -55,7 +55,7 @@ def feature_assort_friend(g, dbname, comname, db_field_names, directed=True):
                       + str(sg.assortativity('foi', 'foi', directed=directed)) + ','
             raw_assort = sg.assortativity('foi', 'foi', directed=directed)
             ass_list = list()
-            for i in xrange(1000):
+            for i in xrange(3000):
                 np.random.shuffle(raw_values)
                 g.vs["foi"] = raw_values
                 vs = g.vs(foi_ge=minv, foi_le=maxv)
@@ -92,19 +92,19 @@ def feature_assort_friend(g, dbname, comname, db_field_names, directed=True):
 def network_stats(dbname, com, fnet, bnet):
     fields = iot.read_fields()
     print 'Following'
-    # fnetwork = gt.load_network(dbname, fnet)
-    # gt.net_stat(fnetwork)
-    # outputs = feature_assort_friend(fnetwork, dbname, com, fields, directed=True)
-    # pickle.dump(outputs, open('data/fnet_assort_all.pick', 'w'))
-    outputs = pickle.load(open('data/fnet_assort_all.pick', 'r'))
+    fnetwork = gt.load_network(dbname, fnet)
+    gt.net_stat(fnetwork)
+    outputs = feature_assort_friend(fnetwork, dbname, com, fields, directed=True)
+    pickle.dump(outputs, open('data/fnet_assort_all.pick', 'w'))
+    # outputs = pickle.load(open('data/fnet_assort_all.pick', 'r'))
     display(outputs, 101)
     for beh in ['retweet', 'reply', 'mention']:
-        # bnetwork = gt.load_beh_network(dbname, bnet, beh)
         print beh
-        # gt.net_stat(bnetwork)
-        # outputs = feature_assort_friend(bnetwork, dbname, com, fields, directed=True)
-        # pickle.dump(outputs, open('data/'+beh+'_assort_all.pick', 'w'))
-        outputs = pickle.load(open('data/'+beh+'_assort_all.pick', 'r'))
+        bnetwork = gt.load_beh_network(dbname, bnet, beh)
+        gt.net_stat(bnetwork)
+        outputs = feature_assort_friend(bnetwork, dbname, com, fields, directed=True)
+        pickle.dump(outputs, open('data/'+beh+'_assort_all.pick', 'w'))
+        # outputs = pickle.load(open('data/'+beh+'_assort_all.pick', 'r'))
         display(outputs, 101)
 
 
