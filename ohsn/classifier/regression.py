fabd3975e926d5b8b6896a46881784cf24ffa98b
wtgmme <wtgmme@gmail.com>
2016-09-13 12:40:04 +0100

    modified:   regression.py

--- ohsn/classifier/regression.py
+++ ohsn/classifier/regression.py
@@ -9,33 +9,49 @@ from os import path
 sys.path.append(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
 
 import ohsn.util.io_util as iot
-import matplotlib.pyplot as plt
-
 import time
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_svmlight_file
 from sklearn.linear_model import LassoLarsIC, LassoCV
 from sklearn import preprocessing
+import seaborn as sns
 
 
-def parameter_select(train):
+def read_data(train):
     X, y = load_svmlight_file(train)
-    X = X.toarray()
-    X = preprocessing.scale(X)
+    print X.shape, y.shape
+    xmax = np.percentile(y, 97.5)
+    xmin = np.percentile(y, 2.5)
+    """Plot GBMI(y) distribution"""
+    sns.distplot(y)
+    plt.axvline(xmin, linestyle='--', color='k',
+                    label='Min 2.5% Cut-off')
+    plt.axvline(xmax, linestyle='--', color='k',
+                    label='Max 2.5% Cut-off')
+    plt.legend()
+    plt.xlabel('GBMI')
+    plt.ylabel('PDF')
+    plt.show()
+    indeces = np.logical_and(y>=xmin, y<=xmax)
 
+    y = y[indeces]
+    X = X[indeces].toarray()
+    X = preprocessing.scale(X)
+    print X.shape, y.shape
+    return X, y
+
+def parameter_select(X, y):
+    print X.shape, y.shape
     ##############################################################################
     # LassoLarsIC: least angle regression with BIC/AIC criterion
-
     # model_bic = LassoLarsIC(criterion='bic')
     # model_bic.fit(X, y)
     # alpha_bic_ = model_bic.alpha_
-
-    model_aic = LassoLarsIC(criterion='aic')
+    model_aic = LassoLarsIC(criterion='aic', max_iter=100000000)
     model_aic.fit(X, y)
     alpha_aic_ = model_aic.alpha_
     print alpha_aic_
-    # print model_aic.coef_
 
     def plot_ic_criterion(model, name, color):
         alpha_ = model.alpha_
@@ -59,21 +75,17 @@ def parameter_select(train):
     for i in xrange(len(fields)):
         print str(fields[i]) +'\t'+ str(model_aic.coef_[i])
 
-def msepath(train):
-    X, y = load_svmlight_file(train)
-    X = X.toarray()
-    X = preprocessing.scale(X)
+
+def msepath(X, y):
+    print X.shape, y.shape
     # Compute paths
     print("Computing regularization path using the coordinate descent lasso...")
-    t1 = time.time()
     model = LassoCV(cv=10, max_iter=3000).fit(X, y)
-    t_lasso_cv = time.time() - t1
 
     # Display results
     m_log_alphas = -np.log10(model.alphas_)
 
     plt.figure()
-    # ymin, ymax = 100, 15000
     plt.plot(m_log_alphas, model.mse_path_, ':')
     plt.plot(m_log_alphas, model.mse_path_.mean(axis=-1), 'k',
              label='Average across the folds', linewidth=2)
@@ -81,13 +93,10 @@ def msepath(train):
                 label='alpha: CV estimate')
 
     plt.legend()
-
     plt.xlabel('-log(alpha)')
     plt.ylabel('Mean square error')
-    plt.title('Mean square error on each fold: coordinate descent '
-              '(train time: %.2fs)' % t_lasso_cv)
+    plt.title('Mean square error on each fold: coordinate descent')
     plt.axis('tight')
-    # plt.ylim(ymin, ymax)
     plt.show()
 
     fields = iot.read_fields()
@@ -96,5 +105,7 @@ def msepath(train):
 
 
 if __name__ == '__main__':
-    parameter_select('data/gbmi.data')
-    msepath('data/gbmi.data')
+    X, y = read_data('data/gbmi.data')
+    parameter_select(X, y)
+    print '---------------------------------'
+    msepath(X, y)
