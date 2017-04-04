f94ff492492079b0f06fe0c8ebb52916fa40ed11
wtgme <wtgmme@gmail.com>
2017-04-04 23:38:49 +0100

    finalize data prepare

--- ohsn/time_series/survival.py
+++ ohsn/time_series/survival.py
@@ -14,24 +14,44 @@ from ohsn.util import db_util as dbt
 from datetime import datetime
 import numpy as np
 import pandas as pd
+import ohsn.util.io_util as iot
+
+def diff_month(d1, d2):
+    return (d1.year - d2.year)*12 + d1.month - d2.month
 
 
 def read_user_time():
     com = dbt.db_connect_col('fed', 'scom')
+    fields = iot.read_fields()
+    trimed_fields = [field.split('.')[-1] for field in fields]
+
+
     data = []
+
     for user in com.find({'liwc_anal.result.WC': {'$exists': True}}, no_cursor_timeout=True):
         if 'status' in user:
             created_at = datetime.strptime(user['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
             scraped_at = user['scrape_timeline_at']
             last_post = datetime.strptime(user['status']['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
-            data.append([user['id_str'], created_at, last_post, scraped_at, 'ED'])
+            if diff_month(scraped_at, last_post) > 2:
+                death = 1
+            else:
+                death = 0
+            values = iot.get_fields_one_doc(user, fields)
+            data.append([user['id_str'], created_at, last_post, scraped_at, 'ED', death].extend(values))
+
     com = dbt.db_connect_col('random', 'scom')
     for user in com.find({'liwc_anal.result.WC': {'$exists': True}}, no_cursor_timeout=True):
         if 'status' in user:
             created_at = datetime.strptime(user['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
             scraped_at = user['scrape_timeline_at']
             last_post = datetime.strptime(user['status']['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
-            data.append([user['id_str'], created_at, last_post, scraped_at, 'RD'])
+            if diff_month(scraped_at, last_post) > 2:
+                death = 1
+            else:
+                death = 0
+            values = iot.get_fields_one_doc(user, fields)
+            data.append([user['id_str'], created_at, last_post, scraped_at, 'RD', death].extend(values))
 
     com = dbt.db_connect_col('younger', 'scom')
     for user in com.find({'liwc_anal.result.WC': {'$exists': True}}, no_cursor_timeout=True):
@@ -39,11 +59,17 @@ def read_user_time():
             created_at = datetime.strptime(user['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
             scraped_at = user['scrape_timeline_at']
             last_post = datetime.strptime(user['status']['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
-            data.append([user['id_str'], created_at, last_post, scraped_at, 'YG'])
+            if diff_month(scraped_at, last_post) > 2:
+                death = 1
+            else:
+                death = 0
+            values = iot.get_fields_one_doc(user, fields)
+            data.append([user['id_str'], created_at, last_post, scraped_at, 'YG', death].extend(values))
 
-    df = pd.DataFrame(data, columns=['uid', 'created_at', 'last_post', 'scraped_at', 'type'])
-    df.to_csv('user-key-date.csv')
+    df = pd.DataFrame(data, columns=['uid', 'created_at', 'last_post', 'scraped_at', 'group', 'death'].extend(trimed_fields))
+    df.to_csv('user-durations.csv')
+
 if __name__ == '__main__':
 
     read_user_time()
-
+    # print diff_month(datetime(2010, 10,1), datetime(2010,9,1))
