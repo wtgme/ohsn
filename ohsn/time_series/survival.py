20f4dcd62f41f0d640868ad90077adf7138ee540
wtgme <wtgmme@gmail.com>
2017-04-23 23:54:03 +0100

    out survival files

--- ohsn/time_series/survival.py
+++ ohsn/time_series/survival.py
@@ -16,6 +16,7 @@ import numpy as np
 import pandas as pd
 import ohsn.util.io_util as iot
 from lifelines.utils import datetimes_to_durations
+import ohsn.util.graph_util as gt
 
 def diff_month(d1, d2):
     return (d1.year - d2.year)*12 + d1.month - d2.month
@@ -25,67 +26,105 @@ def diff_day(d2, d1):
     delta = d2 - d1
     return delta.days
 
-def read_user_time():
+
+def read_user_time(filename):
     fields = iot.read_fields()
     trimed_fields = [field.split('.')[-1] for field in fields]
+    groups = [
+         ('ED', 'fed', 'com', {'liwc_anal.result.WC': {'$exists': True}, 'level': 1}),
+         ('RD', 'random', 'scom', {'liwc_anal.result.WC': {'$exists': True}}),
+         ('YG', 'younger', 'scom', {'liwc_anal.result.WC': {'$exists': True}})
+    ]
+
     data = []
+    for tag, dbname, comname, filter_values in groups:
+        com = dbt.db_connect_col(dbname, comname)
 
-    com = dbt.db_connect_col('fed', 'com')
-    for user in com.find({'liwc_anal.result.WC': {'$exists': True}, 'level': 1}, no_cursor_timeout=True):
-        if 'status' in user:
-            created_at = datetime.strptime(user['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
-            scraped_at = user['scrape_timeline_at']
-            last_post = datetime.strptime(user['status']['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
-            life_time = diff_day(last_post, created_at)
-            average_time = float(life_time)/min(1, user['statuses_count'])
-            longest_tweet_intervalb = user['longest_tweet_interval']
+        for user in com.find(filter_values, no_cursor_timeout=True):
+            if 'status' in user:
+                created_at = datetime.strptime(user['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
+                scraped_at = user['scrape_timeline_at']
+                last_post = datetime.strptime(user['status']['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
+                life_time = diff_day(last_post, created_at)
+                average_time = float(life_time)/min(1, user['statuses_count'])
+                longest_tweet_intervalb = user['longest_tweet_interval']
 
-            observation_interval = diff_day(scraped_at, last_post)
-            if (observation_interval-longest_tweet_intervalb) > 30:
-                death = 1
-            else:
-                death = 0
-            values = iot.get_fields_one_doc(user, fields)
-            data.append([user['id_str'], created_at, last_post, scraped_at, average_time, longest_tweet_intervalb, observation_interval, 'ED',death]+values)
+                observation_interval = diff_day(scraped_at, last_post)
+                if (observation_interval-longest_tweet_intervalb) > 30:
+                    death = 1
+                else:
+                    death = 0
+                values = iot.get_fields_one_doc(user, fields)
+                data.append([user['id_str'], created_at, last_post, scraped_at, average_time,
+                             longest_tweet_intervalb, observation_interval, tag, death] + values)
 
-    com = dbt.db_connect_col('random', 'scom')
-    for user in com.find({'liwc_anal.result.WC': {'$exists': True}}, no_cursor_timeout=True):
-        if 'status' in user:
-            created_at = datetime.strptime(user['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
-            scraped_at = user['scrape_timeline_at']
-            last_post = datetime.strptime(user['status']['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
-            # life_time = diff_day(last_post, created_at)
-            # average_time = float(life_time)/min(1, user['statuses_count'])
-            longest_tweet_intervalb = user['longest_tweet_interval']
-            observation_interval = diff_day(scraped_at, last_post)
-            if (observation_interval-longest_tweet_intervalb) > 30:
-                death = 1
-            else:
-                death = 0
-            values = iot.get_fields_one_doc(user, fields)
-            data.append([user['id_str'], created_at, last_post, scraped_at, average_time, longest_tweet_intervalb, observation_interval, 'RD',death]+values)
+    df = pd.DataFrame(data, columns=['uid', 'created_at', 'last_post', 'scraped_at', 'average_time',
+                                     'longest_time_interval', 'observation_interval', 'group',
+                                     'event'] + trimed_fields)
+    df.to_csv(filename)
 
 
-    com = dbt.db_connect_col('younger', 'scom')
-    for user in com.find({'liwc_anal.result.WC': {'$exists': True}}, no_cursor_timeout=True):
-        if 'status' in user:
-            created_at = datetime.strptime(user['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
-            scraped_at = user['scrape_timeline_at']
-            last_post = datetime.strptime(user['status']['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
-            # life_time = diff_day(last_post, created_at)
-            # average_time = float(life_time)/min(1, user['statuses_count'])
-            longest_tweet_intervalb = user['longest_tweet_interval']
-            observation_interval = diff_day(scraped_at, last_post)
-            if (observation_interval-longest_tweet_intervalb) > 30:
-                death = 1
-            else:
-                death = 0
-            values = iot.get_fields_one_doc(user, fields)
-            data.append([user['id_str'], created_at, last_post, scraped_at, average_time, longest_tweet_intervalb, observation_interval, 'YG',death]+values)
+def read_user_time_iv(filename):
+    fields = iot.read_fields()
+    trimed_fields = [field.split('.')[-1] for field in fields]
+    groups = [
+         ('ED', 'fed', 'com', {'liwc_anal.result.WC': {'$exists': True}, 'level': 1}),
+         ('RD', 'random', 'scom', {'liwc_anal.result.WC': {'$exists': True}}),
+         ('YG', 'younger', 'scom', {'liwc_anal.result.WC': {'$exists': True}})
+    ]
 
+    data = []
+    for tag, dbname, comname, filter_values in groups:
+        com = dbt.db_connect_col(dbname, comname)
+        network1 = gt.Graph.Read_GraphML(tag.lower()+'-net.graphml')
+        gt.summary(network1)
 
-    df = pd.DataFrame(data, columns=['uid', 'created_at', 'last_post', 'scraped_at', 'average_time', 'longest_time_interval', 'observation_interval', 'group', 'event']+trimed_fields)
-    df.to_csv('user-durations.csv')
+        for user in com.find(filter_values, no_cursor_timeout=True):
+            if 'status' in user:
+                created_at = datetime.strptime(user['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
+                scraped_at = user['scrape_timeline_at']
+                last_post = datetime.strptime(user['status']['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
+                life_time = diff_day(last_post, created_at)
+                average_time = float(life_time)/min(1, user['statuses_count'])
+                longest_tweet_intervalb = user['longest_tweet_interval']
+
+                observation_interval = diff_day(scraped_at, last_post)
+                if (observation_interval-longest_tweet_intervalb) > 30:
+                    death = 1
+                else:
+                    death = 0
+                values = iot.get_fields_one_doc(user, fields)
+
+                '''Get friends' profiles'''
+                uid = user['id']
+                exist = True
+                try:
+                    v = network1.vs.find(name=str(uid))
+                except ValueError:
+                    exist = False
+                if exist:
+                    friends = set(network1.successors(str(uid)))
+                    if len(friends) > 0:
+                        friend_ids = [int(network1.vs[vi]['name']) for vi in friends] # return id
+                        print uid in friend_ids
+                        print len(friend_ids)
+                        fatts = []
+                        for fid in friend_ids:
+                            fu = com.find_one({'id': fid, 'liwc_anal.result.WC':{'$exists':True}})
+                            if fu != None:
+                                fatt = iot.get_fields_one_doc(fu, fields)
+                                fatts.append(fatt)
+                        if len(fatts) > 0:
+                            fatts = np.array(fatts)
+                            fmatts = np.mean(fatts, axis=0)
+                            values.extend(fmatts)
+                            data.append([user['id_str'], created_at, last_post, scraped_at, average_time,
+                             longest_tweet_intervalb, observation_interval, tag, death] + values)
+
+    df = pd.DataFrame(data, columns=['uid', 'created_at', 'last_post', 'scraped_at',
+                                     'average_time', 'longest_time_interval', 'observation_interval',
+                                     'group', 'event'] + trimed_fields + ['f_'+tf for tf in trimed_fields])
+    df.to_csv(filename)
 
 
 def count_longest_tweeting_period(dbname, timename, comname):
@@ -110,8 +149,9 @@ if __name__ == '__main__':
 
     # print diff_day(datetime(2010, 10,1), datetime(2010,9,1))
     # from lifelines.utils import k_fold_cross_validation
-    count_longest_tweeting_period('fed', 'timeline', 'com')
-    count_longest_tweeting_period('random', 'timeline', 'scom')
-    count_longest_tweeting_period('younger', 'timeline', 'scom')
-    read_user_time()
+    # count_longest_tweeting_period('fed', 'timeline', 'com')
+    # count_longest_tweeting_period('random', 'timeline', 'scom')
+    # count_longest_tweeting_period('younger', 'timeline', 'scom')
+    read_user_time('user-durations-2.csv')
+    read_user_time_iv('user-durations-iv-2.csv')
 
